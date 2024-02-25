517480a0196b002e62e2dbd1d57cdb93a56e4027
Clutch-254 <gwadabethwel@gmail.com>
2024-02-25 06:09:53 +0300

    fourth commit

---
+++ plppython1.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = input("What is your age? ")
+location = input("What is your location? ")
+
+print("Happy Birthday",name,".""How old are you?",age,".""Where will you move to?",location,".")
